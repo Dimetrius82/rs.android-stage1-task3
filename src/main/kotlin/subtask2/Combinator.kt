2380f7c3ff877b939f18fc80ebab4c8b4bffa681
Dimetrius82 <yakub.dm@mail.ru>
2020-05-06 19:29:29 +0300

    change task

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,25 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
+        val m = array[0] //number of posters
+        val n = array[1] //available colors
+        var x = 0
+        var combinationsNumber = 1
+
+        fun factorial(number: Int): Long{
+            var numberFactorial = 1L
+            for (i in 1..number) numberFactorial*=i
+            return numberFactorial
+        }
+
+        while (combinationsNumber < m && combinationsNumber != 0){
+            x++
+            combinationsNumber = (factorial(n) / (factorial(n - x) * factorial(x))).toInt()
+            println("cn "+ combinationsNumber)
+        }
+
+        if (combinationsNumber == m) return x
+        else return null
         throw NotImplementedError("Not implemented")
     }
 }
